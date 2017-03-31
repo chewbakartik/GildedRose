4afea299cd1a332d5d145c19b9f06d904be6483e
David Kartik <david.kartik@visioncritical.com>
2017-03-31 14:00:38 -0700

    Adding DBContext and fixing some missing folder references.

---
+++ GildedRose/Data/ApiContext.cs
@@ -0,0 +1,12 @@
+﻿using GildedRose.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GildedRose.Data
+{
+    public class ApiContext : DbContext
+    {
+        public ApiContext(DbContextOptions<ApiContext> options) : base(options) {}
+
+        public DbSet<Item> Items { get; set; }
+    }
+}
--- GildedRose/Startup.cs
+++ GildedRose/Startup.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using GildedRose.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -29,6 +31,7 @@ namespace GildedRose
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<ApiContext>(opt => opt.UseInMemoryDatabase());
             services.AddMvc();
         }
 
