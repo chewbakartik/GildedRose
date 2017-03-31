ba19fe6214fdb2f441bb82fb7e7ae25525e1847f
David Kartik <david.kartik@kaskadia.com>
2017-03-31 01:53:57 -0700

    Initial commit

---
+++ GildedRose/Controllers/ItemsController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GildedRose.Controllers
+{
+    [Route("api/items")]
+    public class ItemsController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Tests/GildedRose.Tests.Unit/Controllers/ItemsControllerTest.cs
@@ -0,0 +1,19 @@
+﻿using GildedRose.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using Xunit;
+
+namespace GildedRose.Tests.Unit.Controllers
+{
+    public class ItemsControllerTest
+    {
+        [Fact]
+        public void Index_ReturnsOkRequest()
+        {
+            var controller = new ItemsController();
+
+            var result = controller.Index();
+
+            Assert.IsType<OkResult>(result);
+        }
+    }
+}
