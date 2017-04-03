f3847faa0009dafb9f1261145d03cf41842c77c9
David Kartik <david.kartik@kaskadia.com>
2017-04-02 21:29:37 -0700

    Adding repository interface

---
+++ GildedRose/Data/Abstract/IEntityBaseRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace GildedRose.Data.Abstract
+{
+    public interface IEntityBaseRepository<T> where T : class
+    {
+        IEnumerable<T> GetAll();
+        Task<IEnumerable<T>> GetAllAsync();
+        T Get(int id);
+        T Get(Expression<Func<T, bool>> predicate);
+        Task<T> GetAsync(int id);
+        IEnumerable<T> FindBy(Expression<Func<T, bool>> predicate);
+        Task<IEnumerable<T>> FindByAsync(Expression<Func<T, bool>> predicate);
+        void Add(T entity);
+        void Delete(T entity);
+        void Edit(T entity);
+        void Commit();
+    }
+}
